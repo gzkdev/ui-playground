0b06ee5b4541d387da26b11b96e4f0eb49126759
gzkdev <ezihegodswill01@gmail.com>
2023-04-05 13:53:54 +0100

    clean up components

--- src/components/DemosGrid/index.tsx
+++ src/components/DemosGrid/index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const DemosGrid = () => {
   return <div className=""></div>;
 };
--- src/components/PageLayout/index.tsx
+++ src/components/PageLayout/index.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import DemosGrid from "../DemosGrid";
 
 const PageLayout = () => {
   return (
     <main>
       <div className="max-w-7xl mx-auto py-10 px-5">
-        <h1 className="pt-5 pb-10 text-5xl font-extrabold">React Playground</h1>
+        <h1 className="pt-5 pb-10 text-5xl font-black">React Playground</h1>
         <DemosGrid />
       </div>
     </main>
