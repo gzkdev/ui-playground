3e3321bb3090a30c53821a57c1aded3dd2496b9b
gzkdev <ezihegodswill01@gmail.com>
2023-04-05 14:02:31 +0100

    add framer-motion library

---
+++ src/components/DemoItem/index.tsx
@@ -0,0 +1,5 @@
+const DemoItem = () => {
+  return <div className="rounded-3xl bg-gray-400 aspect-square relative"></div>;
+};
+
+export default DemoItem;
--- src/components/DemosGrid/index.tsx
+++ src/components/DemosGrid/index.tsx
@@ -1,5 +1,13 @@
+import DemoItem from "../DemoItem";
+
 const DemosGrid = () => {
-  return <div className=""></div>;
+  return (
+    <div className="grid gap-5 lg:grid-cols-3 lg:gap-10">
+      <DemoItem />
+      <DemoItem />
+      <DemoItem />
+    </div>
+  );
 };
 
 export default DemosGrid;
