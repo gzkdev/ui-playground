336c68504ee8e0a1f3785ed6e616195e9cff493d
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-01 02:23:45 +0100

    add select token tray

--- src/app/select-token-tray/page.tsx
+++ src/app/select-token-tray/page.tsx
@@ -1,21 +1,17 @@
 import Link from "next/link";
+import { ArrowElbowUpLeft } from "@phosphor-icons/react/dist/ssr";
 
 export default function SelectTokenTray() {
   return (
     <div className="max-w-screen-md p-6 mx-auto space-y-40">
       <div className="my-10 space-y-2">
         <div className="flex flex-col gap-10">
-          <Link href="/" className="text-sm">
+          <Link
+            href="/"
+            className="p-2 rounded-lg -m-2 bg-zinc-800 self-start hover:opacity-50 transition hover:text-zinc-100"
+          >
             <span className="sr-only">Home</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              fill="currentColor"
-              viewBox="0 0 256 256"
-            >
-              <path d="M200,80V224a8,8,0,0,1-16,0V88H67.31l34.35,34.34a8,8,0,0,1-11.32,11.32l-48-48-.06-.07c-.16-.16-.32-.34-.47-.52l-.23-.31a3.71,3.71,0,0,1-.23-.32l-.23-.37a2.91,2.91,0,0,1-.17-.3c-.07-.12-.13-.25-.19-.38s-.1-.21-.15-.33-.09-.25-.14-.37l-.13-.36-.09-.39c0-.13-.07-.25-.1-.37s0-.31-.06-.46,0-.21-.05-.32a8.34,8.34,0,0,1,0-1.58c0-.11,0-.21.05-.32s0-.31.06-.46.06-.24.1-.37l.09-.39.13-.36c.05-.12.09-.25.14-.37s.1-.22.15-.33.12-.26.19-.38a2.91,2.91,0,0,1,.17-.3l.23-.37a3.71,3.71,0,0,1,.23-.32l.23-.31c.15-.18.31-.36.47-.52l.06-.07,48-48a8,8,0,0,1,11.32,11.32L67.31,72H192A8,8,0,0,1,200,80Z"></path>
-            </svg>
+            <ArrowElbowUpLeft />
           </Link>
           <h2>Select token tray</h2>
         </div>
