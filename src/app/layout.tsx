2cadfc9171ee8499e26f98fba36cff1f5ab9ff14
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-01 04:32:51 +0100

    change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "UI Playground",
-  description: "A personal repository for user interface prototypes",
+  description: "A personal repository for user  face prototypes",
 };
 
 export default function RootLayout({
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={nunitoSans.className}>{children}</body>
     </html>
   );
 }
