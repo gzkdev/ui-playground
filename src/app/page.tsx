55ce26cece38dc7bee130f0e791e6663b8fe2233
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-01 02:04:17 +0100

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ export default function Home() {
   return (
     <div className="max-w-screen-md p-6 mx-auto space-y-40">
       <div className="my-10 space-y-2">
-        <h1 className="text-white">UI Playground</h1>
+        <h1>UI Playground</h1>
         <p>A personal repository for user interface prototypes</p>
       </div>
 
       <div className="space-y-10">
-        <h2 className="text-white">Components</h2>
+        <h2>Components</h2>
 
         <ul>
           <li>
