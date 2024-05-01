b06ea6a7ec05d5f4fb25feebafc36eb74c28aac2
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-01 02:02:03 +0100

    add home page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="max-w-screen-md p-6 mx-auto">
-      <div>
-        <h1>UI Playground</h1>
+    <div className="max-w-screen-md p-6 mx-auto space-y-40">
+      <div className="my-10 space-y-2">
+        <h1 className="text-white">UI Playground</h1>
+        <p>A personal repository for user interface prototypes</p>
+      </div>
+
+      <div className="space-y-10">
+        <h2 className="text-white">Components</h2>
+
+        <ul>
+          <li>
+            <Link
+              href="/"
+              className="flex p-3 -m-3 transition rounded-lg hover:bg-zinc-800 hover:text-white"
+            >
+              Collapsible tray
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
