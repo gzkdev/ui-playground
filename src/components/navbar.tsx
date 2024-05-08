89dc02ddda0f0922e07140c2e66b2f8a3d84da41
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-08 23:25:15 +0100

    add navbar content

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,5 +2,15 @@ import Link from "next/link";
 import React from "react";
 
 export function Navbar() {
-  return <nav></nav>;
+  return (
+    <nav>
+      <div className="max-w-screen-lg mx-auto p-6">
+        <div className="flex items-center justify-between">
+          <Link href="/" className="font-semibold">
+            ui playground
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
