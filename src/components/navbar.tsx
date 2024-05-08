c8d38433df3befcb162143a14dfecc6e4ec688d5
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-08 23:36:05 +0100

    update navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,18 +4,21 @@ import React from "react";
 export function Navbar() {
   return (
     <nav>
-      <div className="max-w-screen-lg mx-auto p-6">
+      <div className="max-w-screen-lg mx-auto p-10">
         <div className="flex items-center justify-between">
-          <Link href="/" className="font-semibold">
+          <Link href="/" className="font-semibold text-zinc-800">
             ui playground
           </Link>
 
-          <ul>
+          <ul className="flex items-center gap-6 text-sm">
             <li>
               <Link href="/">About</Link>
-              <Link href="/">Components</Link>
-              <Link href="/">Github</Link>
             </li>
+            <li>
+              <Link href="/">Components</Link>
+            </li>
+            <Link href="/">Github</Link>
+            <li></li>
           </ul>
         </div>
       </div>
