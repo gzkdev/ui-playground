084a9468f035eddb814f3f65e655bbafc914843c
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-09 17:57:59 +0100

    update home page markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="max-w-screen-md p-6 mx-auto space-y-20">
-      <div className="my-10 space-y-2">
-        <h1>UI Playground</h1>
+    <div className="p-6 space-y-20">
+      <div className="my-10 space-y-2 text-center">
+        <h1 className="text-4xl">UI Playground</h1>
         <p>A personal repository for user interface prototypes</p>
       </div>
-
-      <div className="space-y-6">
-        <h2>Components</h2>
-
+      <div className="max-w-md mx-auto space-y-6 text-sm">
         <ul>
           <li>
             <Link
               href="/select-token-tray"
-              className="flex p-3 -m-3 transition rounded-lg hover:bg-zinc-300 hover:text-zinc-800"
+              className="flex p-2 -m-2 transition rounded-xl border border-transparent hover:bg-zinc-200/50 hover:text-zinc-800 hover:border-zinc-200"
             >
               Select token tray
             </Link>
