ce3659e0754ac82d10facd65c2685c1bce4751e4
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-14 12:55:36 +0100

    remove navbar from home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/navbar";
 
 const nunito = Inter({ subsets: ["latin"] });
 
@@ -17,10 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={nunito.className}>
-        <Navbar />
-        {children}
-      </body>
+      <body className={nunito.className}>{children}</body>
     </html>
   );
 }
